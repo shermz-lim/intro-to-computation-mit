ed42d8a07ae2f24ef5fc23992e12a668f1677723
Sherman Lim <shermz.lim.jh@gmail.com>
2019-01-15 15:42:27 +0800

    Added bisection search exercise for sqrt nums

---
+++ bisectionSqrt.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3 
+# Implements bisection search to find the square root of a number x 
+
+x = 0 
+while x <= 0:
+    x = int(input('Find the square root of:'))
+    
+epsilon = 0.00001
+low = 0 
+high = max(1.0, x) 
+guess = (high + low)/2
+
+while abs(guess**2 - x) >= epsilon:
+    if guess**2 > x:
+        high = guess 
+    else: 
+        low = guess 
+    guess = (high + low)/2
+
+print("The square root of {} is approximately {}".format(x, guess))            
