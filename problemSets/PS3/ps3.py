e1fa72d5b949db30667c666d06ffaf105efe2cec
Sherman Lim <shermz.lim.jh@gmail.com>
2019-01-22 19:30:54 +0800

    added part 5 of ps3

--- problemSets/PS3/ps3.py
+++ problemSets/PS3/ps3.py
@@ -280,38 +280,45 @@ def play_hand(hand, word_list):
       
     """
     
-    # BEGIN PSEUDOCODE <-- Remove this comment when you implement this function
     # Keep track of the total score
-    
+    total_score = 0 
     # As long as there are still letters left in the hand:
-    
+    while sum(hand.values()) > 0: 
+        n = sum(hand.values())
         # Display the hand
-        
+        print("Current Hand:", end=" ")
+        display_hand(hand)
         # Ask user for input
-        
+        word = input("Enter word, or '!!' to indicate that you are finished: ")
         # If the input is two exclamation points:
-        
+        if word == "!!":
             # End the game (break out of the loop)
-
+            break 
             
         # Otherwise (the input is not two exclamation points):
-
+        else:
             # If the word is valid:
-
+            if is_valid_word(word, hand, word_list):
                 # Tell the user how many points the word earned,
                 # and the updated total score
-
+                score = get_word_score(word, n)
+                total_score += score
+                print('"{}" earned {} points. Total: {} points'.format(word, score, total_score))
             # Otherwise (the word is not valid):
+            else:
                 # Reject invalid word (print a message)
-                
+                print("That is not a valid word. Please choose another word.")
             # update the user's hand by removing the letters of their inputted word
-            
+            hand = update_hand(hand, word)
 
     # Game is over (user entered '!!' or ran out of letters),
     # so tell user the total score
+    if sum(hand.values()) == 0:
+        print("Ran out of letters.", end=" ")
+    print("Total score: {}".format(total_score))    
 
     # Return the total score as result of function
-
+    return total_score
 
 
 #
